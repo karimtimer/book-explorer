9690778b16be99257cf21cf745f144c2923a86d4
Karim Timer <abdikarimtimer@gmail.com>
2020-10-19 08:12:17 +0100

    update upload text

---
+++ app/modules/external_service.rb
@@ -0,0 +1,9 @@
+module ExternalService
+  def make_external_post(upload_url)
+    uri = URI.parse('https://enbrtwpe490a7.x.pipedream.net')
+    request = Net::HTTP::Post.new(uri)
+    request.set_form_data('s3_url' => upload_url)
+    req_options = { use_ssl: uri.scheme == 'https' }
+    Net::HTTP.start(uri.hostname, uri.port, req_options) { |http| http.request(request) }
+  end
+end
